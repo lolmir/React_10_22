ac531d286d3b3a41835f6154d701464ef63b070b
lolixa <lolixa9002@gmail.com>
2022-11-09 21:48:05 +0300

    2HW

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,7 @@
-export const Message = ({ name }) => {
-	return <div>
-		<h1>
-			Hello!
-			<br></br>
-			My name is {name},
-			<br></br>
-			Its my first page</h1>
-	</div>
-}
+export const Message = (props) => {
+	return (
+		<div>
+			{props.name}
+		</div>
+	);
+};
